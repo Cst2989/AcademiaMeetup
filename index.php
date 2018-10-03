5482c1ac9bf579ff1dab5e4cc81c48e186a2fbaf
Neciu Dan <dan@adoreme.com>
2018-10-03 16:38:13 -0400

    first commit

---
+++ index.php
@@ -0,0 +1,182 @@
+<html>
+    <head>
+        <title>Academia Testarii Metup</title>
+        <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.1.3/css/bootstrap.min.css" integrity="sha384-MCw98/SFnGE8fJT3GXwEOngsV7Zt27NXFoaoApmYm81iuXoPkFOJwJ8ERdknLPMO" crossorigin="anonymous">
+        <link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.3.1/css/all.css" integrity="sha384-mzrmE5qonljUremFsqc01SB46JvROS7bZs3IO2EmfFsd15uHvIt+Y8vEf7N7fWAU" crossorigin="anonymous">
+        <link rel="stylesheet" href="style.css">
+    </head>
+    <body>
+        <header>
+            <div class="top_bar">
+                <i class="fa fa-phone" style="margin: 10px;"></i> 0799.005.004 sau 0734.540.913
+                <ul class="pull-right text-end social-icons clearfix">
+                    <li><a href="https://www.facebook.com/academiatestarii/" title="Facebook" target="_blank" rel="noopener"><i class="fab fa-facebook-f"></i></a></li><li><a href="https://www.linkedin.com/company/18151104/" title="LinkedIn" target="_blank" rel="noopener"><i class="fab fa-linkedin-in"></i></a></li>
+                </ul>
+            </div>
+            <nav class="navbar navbar-light">
+                <span class="navbar-brand mb-0 h1">
+                    <img height="111" src="http://academiatestarii.ro/wp/wp-content/uploads/2017/11/Logo-color-RGB.png" alt="">
+                </span>
+            </nav>
+        </header>
+        <div class="container">
+            <div id="failed" class="row">
+                <div class="col">
+                    <h2> Ne pare rau, comanda dumneavostra nu a putut fi procesata. Incercati din nou.</h2>
+                    <button id="reload" class="btn btn-primary mx-auto" style="width: 200px;display: block">Incearca din nou</button>
+                </div>
+            </div>
+            <div id="success" class="row">
+                <div class="col">
+                    <h3><i class="fas fa-users"></i> Rezumat Comanda</h3>
+                    <table id="cart-final" class="table table-hover table-condensed">
+                        <thead>
+                        <tr>
+                            <th style="width:20%">Product</th>
+                            <th style="width:20%">Price</th>
+                            <th style="width:18%">Quantity</th>
+                            <th style="width:22%" class="text-center">Subtotal</th>
+                        </tr>
+                        </thead>
+                        <tbody>
+
+                        </tbody>
+                        <tfoot>
+                        <tr>
+                            <td class="hidden"><a href="#" class="btn btn-warning hidden-xl" style="visibility: hidden"><i class="fa fa-angle-left"></i> Continue Shopping</a></td>
+                            <td colspan="2">Discount <span id="final-discount"></span>%</td>
+                            <td class="hidden-xs text-center"><strong>Total <span id="final-total"></span>$</strong></td>
+                        </tr>
+                        </tfoot>
+                    </table>
+                </div>
+            </div>
+            <div id="step-1" class="row">
+                <div class="col-8">
+                    <form id="personal_form">
+                        <h3><i class="fas fa-users"></i> Informatii Personale</h3>
+                        <div class="row">
+                            <div class="col">
+                                <div class="form-group">
+                                    <label for="nume">Nume</label>
+                                    <input type="text" class="form-control" id="nume" placeholder="Nume">
+                                </div>
+                                <div class="form-group">
+                                    <label for="email">Email</label>
+                                    <input type="email" class="form-control" id="email" aria-describedby="emailHelp" placeholder="Email">
+                                </div>
+                                <div class="form-group">
+                                    <label for="varsta">Varsta</label>
+                                    <input type="text" class="form-control" id="varsta" placeholder="Varsta">
+                                </div>
+                                <div class="form-group">
+                                    <label for="studii">Studii Absolvite</label>
+                                    <input type="text" class="form-control" id="studii" placeholder="Studii Absolvite">
+                                </div>
+                            </div>
+                            <div class="col">
+                                <div class="form-group">
+                                    <label for="prenume">Prenume</label>
+                                    <input type="text" class="form-control" id="prenume" placeholder="Prenume">
+                                </div>
+                                <div class="form-group">
+                                    <label for="telefon">Telefon</label>
+                                    <input type="text" class="form-control" id="telefon" placeholder="Telefon">
+                                </div>
+                                <div class="form-group">
+                                    <label for="localitate">Localitate</label>
+                                    <input type="text" class="form-control" id="localitate" placeholder="Localitate">
+                                </div>
+                                <div class="form-group">
+                                    <label for="judet">Judet</label>
+                                    <input type="text" class="form-control" id="judet" placeholder="Judet">
+                                </div>
+                            </div>
+                        </div>
+                        <div class="row">
+                            <div class="col">
+                                <button id="submit-personal" class="btn btn-primary mx-auto" style="width: 200px;display: block">Continua cumparaturiile</button>
+                            </div>
+                        </div>
+                    </form>
+                    <form id="shop-form">
+                        <h3><i class="fas fa-shopping-bag"></i> Produse</h3>
+                        <div class="row">
+                            <div class="col">
+                                <div class="form-group">
+                                    <label for="categorie">Categorie</label>
+                                    <select class="form-control" id="categorie">
+                                        <option value="1">Categoria 1</option>
+                                        <option value="2">Categoria 2</option>
+                                        <option value="3">Categoria 3</option>
+                                        <option value="4">Categoria 4</option>
+                                        <option value="5">Categoria 5</option>
+                                    </select>
+                                </div>
+                                <div class="form-group">
+                                    <label for="disponobil">Cantitate Disponibila</label>
+                                    <input type="text" class="form-control" id="disponobil" readonly>
+                                </div>
+                            </div>
+                            <div class="col">
+                                <div class="form-group">
+                                    <label for="produs">Produs</label>
+                                    <select class="form-control" id="produs">
+                                        <option value="1">Produs 1</option>
+                                        <option value="2">Produs 2</option>
+                                        <option value="3">Produs 3</option>
+                                        <option value="4">Produs 4</option>
+                                        <option value="5">Produs 5</option>
+                                    </select>
+                                </div>
+                                <div class="form-group">
+                                    <label for="pret">Pret</label>
+                                    <input type="text" class="form-control" id="pret" readonly>
+                                </div>
+                            </div>
+                            <div class="col">
+                                <div class="form-group">
+                                    <label for="cantitate">Cantitate</label>
+                                    <input type="number" class="form-control" id="cantitate" placeholder="Cantitate">
+                                </div>
+                                <div class="form-group">
+                                    <button type="submit" id="adauga" class="btn btn-primary mx-auto" style="margin-top:48px;width: 200px;display: block"><i class="fas fa-plus"></i> Adauga in cos</button>
+                                </div>
+                            </div>
+                        </div>
+                    </form>
+                </div>
+                <div class="col-4">
+                    <h3 class="right"><i class="fas fa-cart-plus"></i> Shopping Cart</h3>
+                    <div id="cart-content">
+                        <table id="cart" class="table table-hover table-condensed">
+                            <thead>
+                            <tr>
+                                <th style="width:20%">Product</th>
+                                <th style="width:20%">Price</th>
+                                <th style="width:18%">Quantity</th>
+                                <th style="width:22%" class="text-center">Subtotal</th>
+                                <th style="width:20%"></th>
+                            </tr>
+                            </thead>
+                            <tbody>
+
+                            </tbody>
+                            <tfoot>
+                            <tr>
+                                <td class="hidden"><a href="#" class="btn btn-warning hidden-xl" style="visibility: hidden"><i class="fa fa-angle-left"></i> Continue Shopping</a></td>
+                                <td colspan="2">Discount <span id="discount"></span>%</td>
+                                <td class="hidden-xs text-center"><strong >Total <span id="total"></span>$</strong></td>
+                                <td><a href="#" id="checkout" class="btn btn-success btn-block">Checkout <i class="fa fa-angle-right"></i></a></td>
+                            </tr>
+                            </tfoot>
+                        </table>
+                    </div>
+                </div>
+            </div>
+
+        </div>
+        <script   src="https://code.jquery.com/jquery-3.3.1.min.js"   integrity="sha256-FgpCb/KJQlLNfOu91ta32o/NMZxltwRo8QtmkMRdAu8="   crossorigin="anonymous"></script>
+        <script src="scripts.js" type="text/javascript"></script>
+    </body>
+</html>
---
+++ scripts.js
@@ -0,0 +1,269 @@
+$( document ).ready(function() {
+    var productsInCart = [];
+    var preturi = {
+      1: [10, 20 , 30, 40 , 50],
+      2: [20 , 30, 40 , 50, 60],
+      3: [10 , 30, 50 , 70, 90],
+      4: [10 , 12, 14 , 17, 18],
+      5: [100 , 120, 140 , 160, 180]
+    };
+    var hasError = false;
+    var discount = 0;
+    var secretDiscount = 0;
+
+    $("#pret").val(getCost());
+    getCantitate();
+    populateCart();
+    function getCantitate()
+    {
+        var category = $("#categorie").val();
+        var produs = $('#produs').val();
+
+        $.ajax({
+            method: "POST",
+            url: "api.php",
+            data: { categorie: category, produs: produs, post: 1 }
+        })
+            .done(function( msg ) {
+                $("#disponobil").val(msg);
+            });
+
+    };
+
+    function addToCart() {
+        var product = createProduct();
+        productsInCart.push(product);
+        populateCart();
+        $("#cantitate").val('');
+    }
+
+    function createProduct() {
+        return {
+            id: productsInCart.length + 1,
+            nume: 'Produs ' + $('#produs').val(),
+            pret: $('#pret').val(),
+            cantitate: $('#cantitate').val(),
+            subtotal: parseInt($('#cantitate').val()) * parseInt($('#pret').val())
+        }
+    }
+    function populateCart() {
+        $("#cart > tbody").html('');
+        productsInCart.forEach(function(produs) {
+            $( "#cart > tbody" ).append( "<tr>\n" +
+                "                                <td data-th=\"Product\">\n" +
+                "                                    <div class=\"row\">\n" +
+                "                                        <div class=\"col-sm-12\">\n" +
+                "                                            <h4 class=\"nomargin\">" + produs.nume + "</h4>\n" +
+                "                                        </div>\n" +
+                "                                    </div>\n" +
+                "                                </td>\n" +
+                "                                <td data-th=\"Price\">" + produs.pret + " $</td>\n" +
+                "                                <td data-th=\"Quantity\">\n" +
+                "                                    <input type=\"number\"  class=\"form-control quantity text-center\" data-id=" + produs.id + " value=" + produs.cantitate + ">\n" +
+                "                                </td>\n" +
+                "                                <td data-th=\"Subtotal\" class=\"text-center\">"+ produs.subtotal +" $</td>\n" +
+                "                                <td class=\"actions\" data-th=\"\">\n" +
+                "                                    <button class=\"btn btn-danger remove btn-sm\" data-id=" + produs.id + "><i class=\"fas fa-trash-alt\"></i></button>\n" +
+                "                                </td>\n" +
+                "                            </tr>" );
+
+        });
+        setDiscount();
+        setTotal();
+    }
+
+    function populateFinalCart(){
+        productsInCart.forEach(function(produs) {
+            //duplicate logic here
+            $( "#cart-final > tbody" ).append( "<tr>\n" +
+                "                                <td data-th=\"Product\">\n" +
+                "                                    <div class=\"row\">\n" +
+                "                                        <div class=\"col-sm-12\">\n" +
+                "                                            <h4 class=\"nomargin\">" + produs.nume + "</h4>\n" +
+                "                                        </div>\n" +
+                "                                    </div>\n" +
+                "                                </td>\n" +
+                "                                <td data-th=\"Price\">" + produs.pret + " $</td>\n" +
+                "                                <td data-th=\"Quantity\">\n" +
+                "                                    cant. " + produs.cantitate + "\n" +
+                "                                </td>\n" +
+                "                                <td data-th=\"Subtotal\" class=\"text-center\">"+ produs.subtotal +" $</td>\n" +
+                "                            </tr>" );
+
+        });
+        $("#final-discount").html(secretDiscount);
+        setFinalTotal();
+    }
+
+    function setDiscount() {
+        var varsta = $("#varsta").val();
+
+        if(parseInt(varsta) < 56) {
+            discount = 40;
+        }
+        if(parseInt(varsta) < 36) {
+            discount = 30;
+        }
+        if(parseInt(varsta) < 26) {
+            discount = 20;
+        }
+        secretDiscount = discount;
+
+        if($("#studii").val() === '' || $("#telefon").val() === '') {
+            secretDiscount = 0;
+        }
+        if($("#localitate").val() === 'Bucuresti' || $("#localitate").val() === 'Iasi' || $("#localitate").val() === 'Cluj') {
+            secretDiscount = secretDiscount + 10;
+        }
+
+        $("#discount").html(discount);
+
+    }
+
+    function checkout() {
+        var totalQuantity = 0;
+        for (var i = 0; i < productsInCart.length; i++)
+        {
+            totalQuantity += productsInCart[i]['cantitate'];
+        }
+
+        if(totalQuantity > 3) {
+            secretDiscount = secretDiscount + 5;
+        }
+
+        $.ajax({
+            method: "POST",
+            url: "api.php",
+            data: { produse: productsInCart, discount: secretDiscount, post: 2 }
+        })
+            .done(function( msg ) {
+                $("#step-1").hide();
+                if(1) {
+                    $("#success").show();
+                    populateFinalCart();
+                } else {
+                    $("#failed").show();
+                }
+            });
+    }
+    function setTotal() {
+        var total = 0;
+        for (var i = 0; i < productsInCart.length; i++)
+        {
+            total += productsInCart[i]['subtotal'];
+        }
+        total = total - (discount*total) / 100;
+        $("#total").html(total);
+    }
+
+    function setFinalTotal() {
+        var total = 0;
+        for (var i = 0; i < productsInCart.length; i++)
+        {
+            total += productsInCart[i]['subtotal'];
+        }
+        console.log(total);
+        total = total - ((secretDiscount*total) / 100);
+        console.log(total);
+        $("#final-total").html(total);
+    }
+    function getCost(){
+        var category = $("#categorie").val();
+        var produs = $('#produs').val();
+        return preturi[category][produs - 1];
+    }
+
+    function updateQuantity(id, value) {
+        productsInCart = productsInCart.map(function(product) {
+            if(product['id'] == id) {
+                product['cantitate'] = value;
+                product['subtotal'] = parseInt(value) * parseInt(product.pret);
+            }
+            return product
+        });
+        populateCart();
+    }
+    function removeProduct(id) {
+        productsInCart = productsInCart.filter(function(el) { return parseInt(el.id) != id; });
+        populateCart();
+    }
+    $('#cart').on('keyup', '.quantity', function () {
+        var id = $(this).attr('data-id');
+        updateQuantity(id, $(this).val());
+
+    });
+
+    $('#cart').on('click', '.remove', function () {
+        var id = $(this).attr('data-id');
+        removeProduct(id);
+    });
+
+    $("#adauga").on('click', function(){
+        if($("#cantitate").val() == '' && parseInt($("#cantitate").val()) > parseInt($("#disponobil").val())) {
+            $('#cantitate').addClass('has-error');
+        } else {
+            $('#cantitate').removeClass('has-error');
+            addToCart();
+        }
+    });
+
+    $( "#categorie, #produs" ).change(function() {
+        $("#pret").val(getCost());
+        getCantitate();
+    });
+    $("#checkout").on('click', function(){
+        checkout();
+
+    });
+    $("#submit-personal").on('click', function(){
+       if($("#nume").val() === ''){
+           hasError = true;
+           $('#nume').addClass('has-error');
+       } else {
+           $('#nume').removeClass('has-error');
+           hasError = false;
+       }
+        if($("#prenume").val() === ''){
+            hasError = true;
+            $('#prenume').addClass('has-error');
+        } else {
+            $('#prenume').removeClass('has-error');
+            hasError = false;
+        }
+        if($("#email").val() === ''){
+            hasError = true;
+            $('#email').addClass('has-error');
+        } else {
+            $('#email').removeClass('has-error');
+            hasError = false;
+        }
+        if($("#varsta").val() === ''){
+            hasError = true;
+            $('#varsta').addClass('has-error');
+        } else {
+            $('#varsta').removeClass('has-error');
+            hasError = false;
+        }
+        if($("#localitate").val() === ''){
+            hasError = true;
+            $('#localitate').addClass('has-error');
+        } else {
+            $('#localitate').removeClass('has-error');
+            hasError = false;
+        }
+        if(!hasError) {
+            $("#personal_form").hide();
+            $("#shop-form").addClass('active');
+        }
+    });
+    $("#personal_form").submit(function(e){
+        e.preventDefault(e);
+    });
+    $("#shop-form").submit(function(e){
+        e.preventDefault(e);
+    });
+
+    $("#reload").on('click', function(){
+        window.location.reload();
+    })
+});
